99a05986a158f9bbd8187a5e4ce4c23c01e36747
DashboardApp <dashboard@company.com>
2026-01-16 03:10:54 +0200

    fix: use call_control_app_id instead of voice_application_id

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ async function ringAgent() {
     {
       to: process.env.TELNYX_AGENT_SIP_URI,
       from: process.env.TELNYX_FROM_NUMBER,
-      voice_application_id: process.env.TELNYX_VOICE_APP_ID,
+      call_control_app_id: process.env.TELNYX_VOICE_APP_ID,
     },
     {
       to: process.env.TELNYX_AGENT_SIP_URI,
       from: process.env.TELNYX_FROM_NUMBER,
-      voice_application_id: process.env.TELNYX_VOICE_APP_ID,
+      call_control_app_id: process.env.TELNYX_VOICE_APP_ID,
     },
   ];
 
